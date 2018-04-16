135aae009da7a0beadb72ad8f9c28bbab742049d
TM <tmtam@connect.ust.hk>
2018-04-17 00:23:36 +0800

    update Save And Load ui

---
+++ src/ui/comp3111/DataSaveAndLoad.java
@@ -0,0 +1,25 @@
+package ui.comp3111;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class DataSaveAndLoad {
+	public void saveData(File file) {
+		try (Scanner scanner = new Scanner(file)){
+		
+			
+		}		
+		catch (Exception e){
+		}		
+	}
+	
+	public void loadData(File file) {
+		try (Scanner scanner = new Scanner(file)){
+			
+			
+		}		
+		catch (Exception e){
+		}
+		
+	}
+}
