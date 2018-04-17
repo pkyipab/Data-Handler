ef0d063106b196105e296daafdf724ee00e40c42
TM <tmtam@connect.ust.hk>
2018-04-18 00:23:20 +0800

    renew update ListView method, change dataImport method, finished Save and Load

--- src/ui/comp3111/DataSaveAndLoad.java
+++ src/ui/comp3111/DataSaveAndLoad.java
@@ -1,22 +1,39 @@
 package ui.comp3111;
 
 import java.io.File;
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import core.comp3111.DataTable;
+
 
 public class DataSaveAndLoad {
 	public void saveData(File file) {
-		try (Scanner scanner = new Scanner(file)){
-		
-			
+		try{
+			FileOutputStream  fos = new FileOutputStream(file.getPath());
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            System.out.println(file.getPath());
+            oos.writeObject(Main.allDataSet);
+            oos.flush();
+            oos.close();
 		}		
-		catch (Exception e){
+		catch (FileNotFoundException e){
+		} 
+		catch (IOException e) {
 		}		
 	}
 	
 	public void loadData(File file) {
-		try (Scanner scanner = new Scanner(file)){
-			
-			
+		try{
+			FileInputStream fis = new FileInputStream(file.getPath());
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Main.allDataSet = (ArrayList<DataTable>) ois.readObject();
+            ois.close();
 		}		
 		catch (Exception e){
 		}
