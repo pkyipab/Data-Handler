c59a24e282601ff44cca6d090593237851fff121
jackc <jackc@10.89.191.142>
2018-04-19 00:29:13 +0800

    add function

--- src/ui/comp3111/PlotLineChart.java
+++ src/ui/comp3111/PlotLineChart.java
@@ -20,25 +20,35 @@ public class PlotLineChart {
     private NumberAxis yAxis = new NumberAxis();
     int chartCount;
     DataTable recieved;
+    DataColumn filtedX;
+    DataColumn filtedY;
     
     public PlotLineChart(DataTable dataTable) {
     	this.recieved = dataTable;
     }
     
+    public void selectColumn(DataTable dataTable) {
+    	
+    }
+    
     public void createLineChart(ArrayList<Chart> exist, Map<VBox, Chart> cMap, DataColumn x, DataColumn y) {
+    	
     	chartCount = exist.size();
-    	String cTitle = "Chart " + chartCount;	//First Chart will called Chart 1, then Chart 2, then Chart 3...etc
+    	String cTitle = "Chart " + chartCount;
+    	
     	LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
     	XYChart.Series<Number, Number> series = new Series<Number, Number>();
     	for(int i = 0; i < x.getSize(); i++) {
     		series.getData().add(new Data<Number, Number>((Number)x.getData()[i], (Number)y.getData()[i]));
     		System.out.println("Added the " + i + " row data: X-axis index = " + (Number)x.getData()[i] + ", Y-axis index = " + (Number)y.getData()[i]);
     	}
+    	
     	lineChart.getData().add(series);
     	exist.add(lineChart);
     	VBox chartBox = new VBox(20);
     	chartBox.getChildren().add(new Label(cTitle));
     	cMap.put(chartBox, lineChart);
+    	
     	System.out.println("NEW Line Chart : " + cTitle + " created.");
     }
     
