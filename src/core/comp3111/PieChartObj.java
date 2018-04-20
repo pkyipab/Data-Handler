9d5322590e97e3eb9896a879edc5f6d5507aac5b
jackc <jackc@10.89.191.142>
2018-04-20 13:20:14 +0800

    PieChart can now handle Both Double and Integer

--- src/core/comp3111/PieChartObj.java
+++ src/core/comp3111/PieChartObj.java
@@ -17,10 +17,10 @@ public class PieChartObj extends GeneralChart {
 		pieChartData = FXCollections.observableArrayList();
 		PieChart pieChart = new PieChart(pieChartData);;
 		this.chartName = "Chart " + (dt.getStoredChart().size() + 1) + " [Type = Pie] [Items: " + stringItemsName + ", Quantity: " + numItemsName + "]";
-		pieChart.setTitle(stringItemsName + " Distriubution");
+		pieChart.setTitle(numItemsName + " Distriubution");
 		for(int i = 0; i < number.getSize(); i++) {
-			pieChartData.add(new PieChart.Data((String)textColumn.getData()[i], (int) numColumn.getData()[i]));
-			System.out.println("Added " + ( i + 1 ) + " data. [ Items = " + (String)textColumn.getData()[i] + " ] [ Quantity = " + (int)numColumn.getData()[i] + " ]");
+			pieChartData.add(new PieChart.Data((String)textColumn.getData()[i], ((Number)numColumn.getData()[i]).doubleValue()));
+			System.out.println("Added " + ( i + 1 ) + " data. [ Items = " + (String)textColumn.getData()[i] + " ] [ Quantity = " + ((Number)numColumn.getData()[i]).doubleValue() + " ]");
 		}
 		this.chart = pieChart;
 		System.out.println("[ NEW Line Chart : " + this.chartName + " created ]");
